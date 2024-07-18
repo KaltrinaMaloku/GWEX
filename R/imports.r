957c96ff022a47300d387cd2a380b341fe14c152
KaltrinaMaloku <81575720+KaltrinaMaloku@users.noreply.github.com>
2024-07-18 15:56:23 +0200

    Update imports.r

--- R/imports.r
+++ R/imports.r
@@ -1,6 +1,7 @@
 #' @import doParallel
 #' @import foreach
 #' @import stats
+#' @import lubridate
 #' @importFrom methods is new
 #' @importFrom utils combn setTxtProgressBar txtProgressBar
 #' @importFrom Rcpp evalCpp
